6b6c9a67c18cb9a5e7bbf180e8c55e91bc6d72e9
wasim pathan <wasimpathan2406@gmail.com>
2026-06-02 22:50:55 +0530

    updated progarm

---
+++ new2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class new2{
+	public static void main(String s[]){
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Hello World");
+	System.out.print("hehehehe");
+}
+}
